1237758f1fd4ac806f480f5ffb4101d1691e1134
sjy8336 <sjy8336@gmail.com>
2024-10-04 18:04:43 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,3 +16,22 @@ const colS = new Swiper(".coll_slide", {
         nextEl:'.collection .swiper-button-next',
     },
 })
+const bestS = new Swiper(".best_slide", {
+    slidesPerView: 5,
+    spaceBetween: 15,
+    navigation:{
+        prevEl:'.best .swiper-button-prev',
+        nextEl:'.best .swiper-button-next',
+    },
+    scrollbar: {el: '.best .swiper-scrollbar'},
+})
+const pmStS = new Swiper(".pmSt_slide", {
+    slidesPerView: 3,
+    slidesPerGroup : 3,
+    spaceBetween: 20,
+    navigation:{
+        prevEl:'.pmSt_right .swiper-button-prev',
+        nextEl:'.pmSt_right .swiper-button-next',
+    },
+    scrollbar: {el: '.pmSt_right .swiper-scrollbar'},
+})
