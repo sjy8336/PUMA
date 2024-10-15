2a020dd6fcdf40568038b3a59aa876a20c5c0d60
sjy8336 <sjy8336@gmail.com>
2024-10-15 13:02:16 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,29 +1,13 @@
-/* const gnb = document.querySelectorAll('.gnb li');
-const sub = document.querySelector('.gnb li .depth1');
-$(sub).hide();
-$(gnb).on('mouseover', function(){
-    $(sub).hide();
-    $(this).find('.depth1').stop(true).show(0);
-}) */
-/* 
-$(gnbPM).on('mouseover', function(){
-    $(subMM).hide();
-    $(subBM).hide();
-    $(subPM).stop(true).show(0);
-})
-$(subPM).on('mouseout', function(){$(subPM).stop(true).hide(0);})
-$(gnbMM).on('mouseover', function(){
-    $(subPM).hide();
-    $(subBM).hide();
-    $(subMM).stop(true).show(0);
-})
-$(subMM).on('mouseout', function(){$(subMM).stop(true).hide(0);})
-$(gnbBM).on('mouseover', function(){
-    $(subPM).hide();
-    $(subMM).hide();
-    $(subBM).stop(true).show(0);
-})
-$(subBM).on('mouseout', function(){$(subBM).stop(true).hide(0);}) */
+$('nav .depth1').hide()
+$('nav .gnb_bg').hide()
+$('.gnb li').on('mouseover', function(){
+    $(this).find('.depth1').stop(true).slideDown();
+    $('nav .gnb_bg').stop(true).slideDown();
+});
+$('.gnb li').on('mouseout', function(){
+    $(this).find('.depth1').stop(true).slideUp();
+    $('nav .gnb_bg').stop(true).slideUp();
+});
 $('#Top').on('click', function(){$('html, body').animate({scrollTop : 0}, 500)})
 $(window).on('scroll', function(){
     if(this.pageYOffset > 750){
